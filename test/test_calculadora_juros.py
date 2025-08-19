69b476d79aed1b328da7572aacf381cdd32da64d
matheus fagnani <fagnanimatheusarara20008@gmail.com>
2025-08-19 10:57:06 -0300

    teste de negativos

--- test/test_calculadora_juros.py
+++ test/test_calculadora_juros.py
@@ -2,20 +2,24 @@ import pytest
 from calculadora_juros import calcular_juros_compostos
 
 
-def test_calcular_juros_compostos():
-    # Teste com valores válidos
+def test_calcular_juros_compostos(capital_investido, taxa_juros, tempo):
+    #  valores valido
     capital_investido = 1000.0
     taxa_juros = 5.0
-    tempo = 2
+    tempo = 2  
     juros, montante = calcular_juros_compostos(capital_investido, taxa_juros, tempo)
     assert juros == pytest.approx(102.5, rel=1e-9)
-
     assert montante == pytest.approx(1102.5, rel=1e-9)
-    # Teste com capital investido zero
-    capital_investido = 0.0
-    taxa_juros = 5.0
-    tempo = 2
-    juros, montante = calcular_juros_compostos(capital_investido, taxa_juros, tempo)
-    assert juros == 0.0
-    assert montante == 0.0
- 
+
+    # Teste com capital investido negativo
+    with pytest.raises(ValueError, match="O capital investido não pode ser negativo."):
+        calcular_juros_compostos(-1000.0, taxa_juros, tempo)   
+    # Teste com taxa de juros negativa  
+    with pytest.raises(ValueError, match="A taxa de juros não pode ser negativa."):
+        calcular_juros_compostos(capital_investido, -5.0, tempo)             
+            
+
+
+
+    
+    
